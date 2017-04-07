49619be4bf864c050abad275424a9b7867b6c842
ACJuicyJ <jasontjauw1999@outlook.com>
2017-04-07 17:26:46 +0200

    bitch bow be4 me

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowGraph = new System.Windows.Forms.Button();
+            this.IJsselmonde = new System.Windows.Forms.CheckBox();
+            this.KralingenCrooswijk = new System.Windows.Forms.CheckBox();
+            this.Pernis = new System.Windows.Forms.CheckBox();
+            this.HillegersbergSchiebroek = new System.Windows.Forms.CheckBox();
+            this.Feijenoord = new System.Windows.Forms.CheckBox();
+            this.RotterdamNoord = new System.Windows.Forms.CheckBox();
+            this.RotterdamCentrum = new System.Windows.Forms.CheckBox();
+            this.Rozenburg = new System.Windows.Forms.CheckBox();
+            this.Delfshaven = new System.Windows.Forms.CheckBox();
+            this.PrinsAlexander = new System.Windows.Forms.CheckBox();
+            this.HoekVanHolland = new System.Windows.Forms.CheckBox();
+            this.Overschie = new System.Windows.Forms.CheckBox();
+            this.Hoogvliet = new System.Windows.Forms.CheckBox();
+            this.Charlois = new System.Windows.Forms.CheckBox();
+            this.BarChart = new System.Windows.Forms.CheckBox();
+            this.PieChart = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ShowGraph
+            // 
+            this.ShowGraph.Location = new System.Drawing.Point(578, 153);
+            this.ShowGraph.Name = "ShowGraph";
+            this.ShowGraph.Size = new System.Drawing.Size(119, 34);
+            this.ShowGraph.TabIndex = 0;
+            this.ShowGraph.Text = "Show Graph";
+            this.ShowGraph.UseVisualStyleBackColor = true;
+            this.ShowGraph.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IJsselmonde
+            // 
+            this.IJsselmonde.AutoSize = true;
+            this.IJsselmonde.Location = new System.Drawing.Point(63, 47);
+            this.IJsselmonde.Name = "IJsselmonde";
+            this.IJsselmonde.Size = new System.Drawing.Size(108, 21);
+            this.IJsselmonde.TabIndex = 17;
+            this.IJsselmonde.Text = "IJsselmonde";
+            this.IJsselmonde.UseVisualStyleBackColor = true;
+            this.IJsselmonde.CheckedChanged += new System.EventHandler(this.IJsselmonde_CheckedChanged);
+            // 
+            // KralingenCrooswijk
+            // 
+            this.KralingenCrooswijk.AutoSize = true;
+            this.KralingenCrooswijk.Location = new System.Drawing.Point(63, 88);
+            this.KralingenCrooswijk.Name = "KralingenCrooswijk";
+            this.KralingenCrooswijk.Size = new System.Drawing.Size(153, 21);
+            this.KralingenCrooswijk.TabIndex = 18;
+            this.KralingenCrooswijk.Text = "Kralingen Crooswijk";
+            this.KralingenCrooswijk.UseVisualStyleBackColor = true;
+            // 
+            // Pernis
+            // 
+            this.Pernis.AutoSize = true;
+            this.Pernis.Location = new System.Drawing.Point(63, 166);
+            this.Pernis.Name = "Pernis";
+            this.Pernis.Size = new System.Drawing.Size(70, 21);
+            this.Pernis.TabIndex = 19;
+            this.Pernis.Text = "Pernis";
+            this.Pernis.UseVisualStyleBackColor = true;
+            // 
+            // HillegersbergSchiebroek
+            // 
+            this.HillegersbergSchiebroek.AutoSize = true;
+            this.HillegersbergSchiebroek.Location = new System.Drawing.Point(63, 204);
+            this.HillegersbergSchiebroek.Name = "HillegersbergSchiebroek";
+            this.HillegersbergSchiebroek.Size = new System.Drawing.Size(189, 21);
+            this.HillegersbergSchiebroek.TabIndex = 20;
+            this.HillegersbergSchiebroek.Text = "Hillegersberg Schiebroek";
+            this.HillegersbergSchiebroek.UseVisualStyleBackColor = true;
+            // 
+            // Feijenoord
+            // 
+            this.Feijenoord.AutoSize = true;
+            this.Feijenoord.Location = new System.Drawing.Point(1104, 47);
+            this.Feijenoord.Name = "Feijenoord";
+            this.Feijenoord.Size = new System.Drawing.Size(97, 21);
+            this.Feijenoord.TabIndex = 21;
+            this.Feijenoord.Text = "Feijenoord";
+            this.Feijenoord.UseVisualStyleBackColor = true;
+            this.Feijenoord.CheckedChanged += new System.EventHandler(this.Feijenoord_CheckedChanged);
+            // 
+            // RotterdamNoord
+            // 
+            this.RotterdamNoord.AutoSize = true;
+            this.RotterdamNoord.Location = new System.Drawing.Point(63, 287);
+            this.RotterdamNoord.Name = "RotterdamNoord";
+            this.RotterdamNoord.Size = new System.Drawing.Size(139, 21);
+            this.RotterdamNoord.TabIndex = 22;
+            this.RotterdamNoord.Text = "Rotterdam Noord";
+            this.RotterdamNoord.UseVisualStyleBackColor = true;
+            // 
+            // RotterdamCentrum
+            // 
+            this.RotterdamCentrum.AutoSize = true;
+            this.RotterdamCentrum.Location = new System.Drawing.Point(1104, 88);
+            this.RotterdamCentrum.Name = "RotterdamCentrum";
+            this.RotterdamCentrum.Size = new System.Drawing.Size(153, 21);
+            this.RotterdamCentrum.TabIndex = 23;
+            this.RotterdamCentrum.Text = "Rotterdam Centrum";
+            this.RotterdamCentrum.UseVisualStyleBackColor = true;
+            // 
+            // Rozenburg
+            // 
+            this.Rozenburg.AutoSize = true;
+            this.Rozenburg.Location = new System.Drawing.Point(1104, 127);
+            this.Rozenburg.Name = "Rozenburg";
+            this.Rozenburg.Size = new System.Drawing.Size(100, 21);
+            this.Rozenburg.TabIndex = 24;
+            this.Rozenburg.Text = "Rozenburg";
+            this.Rozenburg.UseVisualStyleBackColor = true;
+            // 
+            // Delfshaven
+            // 
+            this.Delfshaven.AutoSize = true;
+            this.Delfshaven.Location = new System.Drawing.Point(63, 127);
+            this.Delfshaven.Name = "Delfshaven";
+            this.Delfshaven.Size = new System.Drawing.Size(101, 21);
+            this.Delfshaven.TabIndex = 25;
+            this.Delfshaven.Text = "Delfshaven";
+            this.Delfshaven.UseVisualStyleBackColor = true;
+            // 
+            // PrinsAlexander
+            // 
+            this.PrinsAlexander.AutoSize = true;
+            this.PrinsAlexander.Location = new System.Drawing.Point(63, 244);
+            this.PrinsAlexander.Name = "PrinsAlexander";
+            this.PrinsAlexander.Size = new System.Drawing.Size(129, 21);
+            this.PrinsAlexander.TabIndex = 27;
+            this.PrinsAlexander.Text = "Prins Alexander";
+            this.PrinsAlexander.UseVisualStyleBackColor = true;
+            // 
+            // HoekVanHolland
+            // 
+            this.HoekVanHolland.AutoSize = true;
+            this.HoekVanHolland.Location = new System.Drawing.Point(1104, 166);
+            this.HoekVanHolland.Name = "HoekVanHolland";
+            this.HoekVanHolland.Size = new System.Drawing.Size(144, 21);
+            this.HoekVanHolland.TabIndex = 28;
+            this.HoekVanHolland.Text = "Hoek Van Holland";
+            this.HoekVanHolland.UseVisualStyleBackColor = true;
+            // 
+            // Overschie
+            // 
+            this.Overschie.AutoSize = true;
+            this.Overschie.Location = new System.Drawing.Point(1104, 204);
+            this.Overschie.Name = "Overschie";
+            this.Overschie.Size = new System.Drawing.Size(94, 21);
+            this.Overschie.TabIndex = 29;
+            this.Overschie.Text = "Overschie";
+            this.Overschie.UseVisualStyleBackColor = true;
+            // 
+            // Hoogvliet
+            // 
+            this.Hoogvliet.AutoSize = true;
+            this.Hoogvliet.Location = new System.Drawing.Point(1104, 244);
+            this.Hoogvliet.Name = "Hoogvliet";
+            this.Hoogvliet.Size = new System.Drawing.Size(89, 21);
+            this.Hoogvliet.TabIndex = 30;
+            this.Hoogvliet.Text = "Hoogvliet";
+            this.Hoogvliet.UseVisualStyleBackColor = true;
+            // 
+            // Charlois
+            // 
+            this.Charlois.AutoSize = true;
+            this.Charlois.Location = new System.Drawing.Point(1104, 287);
+            this.Charlois.Name = "Charlois";
+            this.Charlois.Size = new System.Drawing.Size(81, 21);
+            this.Charlois.TabIndex = 31;
+            this.Charlois.Text = "Charlois";
+            this.Charlois.UseVisualStyleBackColor = true;
+            // 
+            // BarChart
+            // 
+            this.BarChart.AutoSize = true;
+            this.BarChart.Location = new System.Drawing.Point(471, 360);
+            this.BarChart.Name = "BarChart";
+            this.BarChart.Size = new System.Drawing.Size(90, 21);
+            this.BarChart.TabIndex = 32;
+            this.BarChart.Text = "Bar Chart";
+            this.BarChart.UseVisualStyleBackColor = true;
+            // 
+            // PieChart
+            // 
+            this.PieChart.AutoSize = true;
+            this.PieChart.Location = new System.Drawing.Point(748, 360);
+            this.PieChart.Name = "PieChart";
+            this.PieChart.Size = new System.Drawing.Size(88, 21);
+            this.PieChart.TabIndex = 33;
+            this.PieChart.Text = "Pie Chart";
+            this.PieChart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 491);
+            this.Controls.Add(this.PieChart);
+            this.Controls.Add(this.BarChart);
+            this.Controls.Add(this.Charlois);
+            this.Controls.Add(this.Hoogvliet);
+            this.Controls.Add(this.Overschie);
+            this.Controls.Add(this.HoekVanHolland);
+            this.Controls.Add(this.PrinsAlexander);
+            this.Controls.Add(this.Delfshaven);
+            this.Controls.Add(this.Rozenburg);
+            this.Controls.Add(this.RotterdamCentrum);
+            this.Controls.Add(this.RotterdamNoord);
+            this.Controls.Add(this.Feijenoord);
+            this.Controls.Add(this.HillegersbergSchiebroek);
+            this.Controls.Add(this.Pernis);
+            this.Controls.Add(this.KralingenCrooswijk);
+            this.Controls.Add(this.IJsselmonde);
+            this.Controls.Add(this.ShowGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ShowGraph;
+        private System.Windows.Forms.CheckBox IJsselmonde;
+        private System.Windows.Forms.CheckBox KralingenCrooswijk;
+        private System.Windows.Forms.CheckBox Pernis;
+        private System.Windows.Forms.CheckBox HillegersbergSchiebroek;
+        private System.Windows.Forms.CheckBox Feijenoord;
+        private System.Windows.Forms.CheckBox RotterdamNoord;
+        private System.Windows.Forms.CheckBox RotterdamCentrum;
+        private System.Windows.Forms.CheckBox Rozenburg;
+        private System.Windows.Forms.CheckBox Delfshaven;
+        private System.Windows.Forms.CheckBox PrinsAlexander;
+        private System.Windows.Forms.CheckBox HoekVanHolland;
+        private System.Windows.Forms.CheckBox Overschie;
+        private System.Windows.Forms.CheckBox Hoogvliet;
+        private System.Windows.Forms.CheckBox Charlois;
+        private System.Windows.Forms.CheckBox BarChart;
+        private System.Windows.Forms.CheckBox PieChart;
+    }
+}
+
